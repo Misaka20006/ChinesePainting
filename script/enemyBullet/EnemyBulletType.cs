885587fdf394924cb71cdefef87e431883ae57c7
Misaka20006 <3338279778@qq.com>
2026-07-02 20:49:58 +0800

    test
    test

--- script/enemyBullet/EnemyBulletType.cs
+++
@@ -1,9 +0,0 @@
-using Godot;
-
-[GlobalClass]
-public partial class EnemyBulletType : Resource
-{
-    [Export] public string typeName = "Default";
-    [Export] public PackedScene bulletPrefab;
-    [Export] public int poolSize = 30;
-}
